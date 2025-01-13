6155a6e178b16e080e01637c8ce0f19ab38dea6c
Shahid2-007 <500booksinkindle@gmail.com>
2025-01-13 09:51:24 +0530

    commit

---
+++ lua/plugins/disabled.lua
@@ -0,0 +1,12 @@
+return {
+  { "folke/trouble.nvim", enabled = false },
+  { "ThePrimeagen/harpoon", enabled = false },
+  { "rafamadriz/friendly-snippets", enabled = false },
+  { "levis6991/gitsigns.nvim", enabled = false },
+  { "echasnovski/mini.ai", enabled = false },
+  { "echasnovski/mini.icons", enabled = false },
+  { "windwp/nvim-ts-autotag", enabled = false },
+  { "catppuccin/catppuccin", enabled = false },
+  { "nvim-neo-tree/neo-tree.nvim", enabled = false },
+  { "MagicDuck/grug-far.nvim", enabled = false },
+}
